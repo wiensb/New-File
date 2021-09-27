32e43d7bfbab09f73db0a8c82fc65a0625fb3241
wiensb <90623020+wiensb@users.noreply.github.com>
2021-09-27 12:34:51 -0400

    commit

---
+++ Braeden_Wiens_8760044_AHC2.py
@@ -0,0 +1,10 @@
+word=input("Select any letters from A-Z ")
+word1=''
+for i in word:
+    if(i.isupper())==True:
+        word1+=(i.lower())
+    elif(i.islower())==True:
+        word1+=(i.upper())
+    elif(i.isspace())==True:
+        word1+=i
+print(word1)
