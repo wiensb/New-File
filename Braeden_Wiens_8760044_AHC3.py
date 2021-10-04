1bd4e75c00a4451d4313b50bb155e7931b6c6226
wiensb <90623020+wiensb@users.noreply.github.com>
2021-10-04 13:11:46 -0400

    commit

--- Braeden_Wiens_8760044_AHC3.py
+++ Braeden_Wiens_8760044_AHC3.py
@@ -1,7 +1,9 @@
-x = int(input("Enter from 1-10 here ")) 
-y = 0
-z = 0
-while y <= x:
-    z += y**2
-    y += 2
-print(z)
+user = int(input("Please Enter a number from 1-10 here ")) 
+comp = 0
+output = 0
+
+while comp <= user:
+    output += comp**2
+    comp += 2
+
+print(output)
