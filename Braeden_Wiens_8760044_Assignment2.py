666a8a4bfaf156de3ce7f7dd318ab6917ab1cdfb
wiensb <90623020+wiensb@users.noreply.github.com>
2021-10-20 17:35:11 -0400

    commit

---
+++ Braeden_Wiens_8760044_Assignment2.py
@@ -0,0 +1,8 @@
+print ("Wlecome to Abby's merchandizing.")
+print("Today we have an assortment of polos and t-shirts as well as jeans!")
+print("Our shirts cost 9.99 each and our jeans are 19.99 each.")
+
+print("Would you like to purchase t-shirts or polos today?")
+
+
+    
