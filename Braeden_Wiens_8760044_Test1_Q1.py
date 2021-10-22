ffbc4ffcfed2db8d087eb8ba8b7527ca594f1cd7
wiensb <90623020+wiensb@users.noreply.github.com>
2021-10-22 10:28:29 -0400

    commit

---
+++ Braeden_Wiens_8760044_Test1_Q1.py
@@ -0,0 +1,12 @@
+word=input("Enter first name: ")
+word1=''
+for i in word:
+    if(i.isupper())==True:
+        word1+=(i.lower())
+    elif(i.islower())==True:
+        word1+=(i.upper())
+    elif(i.isspace())==True:
+        word1+=i
+print(word1)
+
+input("ENTER")
