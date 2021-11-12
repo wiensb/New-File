21204f46e20d85b43cca1b557af96e8d17e94d76
wiensb <90623020+wiensb@users.noreply.github.com>
2021-11-12 10:40:18 -0500

    commit

---
+++ Braeden_Wiens_8760044_Test2_Q1.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+n=int(input("Please input an intager: "))
+print(factorial(n))
+
+input("PRESS ENTER")
