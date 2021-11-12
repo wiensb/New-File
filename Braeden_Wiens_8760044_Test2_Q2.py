b0c4de52860589639b78169eb17d15836b24baeb
wiensb <90623020+wiensb@users.noreply.github.com>
2021-11-12 10:41:26 -0500

    commit

---
+++ Braeden_Wiens_8760044_Test2_Q2.py
@@ -0,0 +1,12 @@
+list1 = []
+num = int(input("Enter the number of intagers: "))
+for i in range(1, num + 1):
+    intadd = int(input("Enter an intager: "))
+    list1.append(intadd)
+
+print("The largest intager is:", max(list1))
+
+input("PRESS ENTER")
+
+
+
